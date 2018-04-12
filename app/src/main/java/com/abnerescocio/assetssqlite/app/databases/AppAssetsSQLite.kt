8379bd25859cd8f322115e3bb71ba5ee4fbf6061
Abner Escócio <abner.escocio@gmail.com>
2018-04-12 13:24:33 -0300

    Remove unnecessary method calls

--- app/src/main/java/com/abnerescocio/assetssqlite/app/databases/AppAssetsSQLite.kt
+++ app/src/main/java/com/abnerescocio/assetssqlite/app/databases/AppAssetsSQLite.kt
@@ -1,17 +1,9 @@
 package com.abnerescocio.assetssqlite.app.databases
 
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
 import com.abnerescocio.assetssqlite.lib.AssetsSQLite
 
 class AppAssetsSQLite(context: Context): AssetsSQLite(context, DATABASE_NAME, null, DATABASE_VERSION) {
-    override fun onCreate(db: SQLiteDatabase?) {
-
-    }
-
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-
-    }
 
     companion object {
         const val DATABASE_NAME = "big_database.db"
